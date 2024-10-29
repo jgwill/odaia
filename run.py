d2ce0c1fda6912a79edaf45a2157ad8f94aa3858
JGWill <jgi@jgwill.com>
2024-10-29 05:18:01 -0400

    doc:redist #1

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ================================================== #
-# This file is a part of PYGPT package               #
-# Website: https://pygpt.net                         #
-# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# This file is a part of oDAIa package               #
+# Website: https://odaia.jgwill.com                       #
+# GitHub:  https://github.com/jgwill/odaia   #
 # MIT License                                        #
+# Redistributed: JGwill                              #
 # Created By  : Marcin Szczygliński                  #
 # Updated Date: 2023.12.05 22:00:00                  #
 # ================================================== #
